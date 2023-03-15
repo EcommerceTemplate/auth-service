974a805bd44e619c15a61932a46bcf815182b000
Nicolas Santiago Bakanowski <nicolasbakanowski@gmail.com>
2023-03-15 16:25:48 -0300

    security configurations

--- app/index.js
+++ app/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const connectDB = require('../app/src/config/db');
 
 const app = express();
 
 connectDB();
+
+dotenv.config();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined'));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
