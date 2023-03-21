9213d5d3142e48b2d3d638e21ecacca48ab883b7
Nicolas Santiago Bakanowski <nicolasbakanowski@gmail.com>
2023-03-20 22:18:47 -0300

    [CHORE] add-health-status

--- app/index.js
+++ app/index.js
@@ -23,6 +23,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/health', (req, res) => {
+  // Check the health of your service
+  // ...
+  res.status(200).send('OK')
+})
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
