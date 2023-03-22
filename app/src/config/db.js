f962cbfd7fb533db470a6beab117697e87e4404e
Nicolas Santiago Bakanowski <nicolasbakanowski@gmail.com>
2023-03-22 17:45:56 -0300

    fix enviroment problems

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const env = process.env.NODE_ENV || 'dev';
-const config = require(`../config/config.${env}.js`); 
+const config = require(`../config/config.${env}.js`);
 require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(config.dbUrl, {
+        await mongoose.connect(config.dbUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
