ecd5285c6f05993b73ef358481a592fc58e712a9
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-03-24 22:17:05 -0300

    ADD: encrypt password for new user

--- app/src/controllers/userControllers.js
+++ app/src/controllers/userControllers.js
@@ -3,8 +3,27 @@ const status = require('http-status');
 
 const createUser = async (req, res, next) => {
     try {
+        const userExistsForUsername = await userService.findUserByUsername(req.body.username);
+        const userExistsForEmail = await userService.findUserByEmail(req.body.email);
+
+        if (userExistsForUsername) {
+            res.status(status.BAD_REQUEST).json({
+                message: `User with username ${req.body.username} exists.`
+            });
+            return;
+        }
+        if (userExistsForEmail) {
+            res.status(status.BAD_REQUEST).json({
+                message: `User with email ${req.body.email} exists.`
+            });
+            return;
+        }
+
         const user = await userService.createUser(req.body);
-        res.status(status._CREATED).json(user);
+        const { username, email } = user;
+        res.status(status.CREATED).json({
+            username, email
+        });
     } catch (error) {
         next(error);
     }
