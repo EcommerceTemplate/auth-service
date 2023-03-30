9fd898c55029efda16c0937a9ec709128baa2ea2
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-03-29 23:45:54 -0300

    ADD: docs for auth and create user

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const swaggerUI = require('swagger-ui-express');
 const userRoutes = require('./src/routes/userRoute');
 const authRoutes = require('./src/routes/authRoute')
 
@@ -9,6 +10,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const env = process.env.NODE_ENV || 'dev';
+const swaggerSpect = require('./src/config/swagger-config');
 
 const connectDB = require('../app/src/config/db');
 const configEnv = require(`./src/config/config.${env}.js`);
@@ -23,6 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('combined'));
 app.use(cors());
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpect));
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes)
